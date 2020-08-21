38844fe5ac24972964931b71f00a41107b902b1c
Billy Chepkemoi <cnafayo@gmail.com>
2020-08-21 07:46:14 +0300

    new file created, routing request introduced

--- delani_node_app/server.js
+++ delani_node_app/server.js
@@ -1,11 +1,19 @@
-let http =require('http');
-const { Http2ServerRequest } = require('http2');
+let http = require('http');
+let url = require('url');
+// A function to wrap our server functionality so that we can export it
+let start = function () {
 
-function onRequest(request, response){
-    console.log("Request recieved")
-    response.writeHead(200, {"content-type":"text/plain"});
-    response.write("Hello World")
-    response.end();
+    function onRequest(request, response) {
+          // Extracting the pathname from the url requested
+          let pathname = url.parse(request.url).pathname
+          console.log(pathname)
+        console.log("Request recieved")
+        response.writeHead(200, { "content-type": "text/plain" });
+        response.write("Hello World")
+        response.end();
+    }
+    http.createServer(onRequest).listen(8000);
+
+    console.log("Server has started")
 }
-http.createServer(onRequest).listen(8000);
-console.log("Server has started")
+exports.start = start;
